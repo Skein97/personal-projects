8bba3f6561ed43458682dd2554ced9c425829626
Skein97 <97639014+Skein97@users.noreply.github.com>
2023-01-26 15:24:31 +0000

    New push

---
+++ image-playground/images.py
@@ -0,0 +1,40 @@
+from PIL import Image, ImageFilter
+
+
+
+
+
+
+
+
+# img = Image.open('./astro.jpg')
+# img.thumbnail((400, 400))
+# img.save('thumbnail2.jpg')
+#
+# print(img.size)
+
+
+# new_img = img.resize((400, 400))
+# new_img.save('thumbnail.jpg')
+
+# img = Image.open('./Pokedex/pikachu.jpg')
+# converted_image = img.convert('L')
+# box = (100, 100, 400, 400)
+# crop = converted_image.crop(box)
+# crop.save('grey4.png', 'png')
+# crop.show()
+
+
+# resize = converted_image.resize((300, 300))
+# resize.save("grey3.png", 'png')
+# resize.show()
+
+# crooked = converted_image.rotate(90)
+# crooked.save("grey2.png", 'png')
+# crooked.show()
+
+# filtered_img = img.filter(ImageFilter.BLUR)
+# filtered_img.save("blur.png", 'png')
+# filtered_img.show()
+
+
