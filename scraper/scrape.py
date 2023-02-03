95608b9123a8d7a414aaad0c2057a1b4d8e18ced
Skein97 <97639014+Skein97@users.noreply.github.com>
2023-02-03 23:55:43 +0000

    Webscraper for Hackernews

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,21 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
+from pprint import pprint
 
 # Create a response variable that gets a webpage
 res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
 
 # Use Beautiful soup to parse the webpage
-soup = BeautifulSoup(res.text, 'html.parser')
-# Select parts of webpage
-links = soup.select('.titleline')
-votes = soup.select('.score')
+soup1 = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
+# Get the webpage attributes
+link1 = soup1.select('.titleline > a')
+link2 = soup2.select('.titleline > a')
+subtext1 = soup1.select('.subtext')
+subtext2 = soup2.select('.subtext')
 
-def create_custom(links, votes):
+# Add all the links together
+links = link1 + link2
+subtexts = subtext1 + subtext2
+
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
+
+def create_custom(links, subtexts):
     hn = []
     for idx, item in enumerate(links):
         title = links[idx].getText()
         href = links[idx].get('href', None)
-        hn.append(href)
-    return hn
+        vote = subtexts[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_votes(hn)
 
-print(create_custom(links, votes))
+
+for i in create_custom(links, subtexts):
+    pprint(i)
